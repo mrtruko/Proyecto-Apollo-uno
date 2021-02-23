69de94c06539e7d7d93422a96d44069ffe521c67
Nicolas Gonzalez <nicolas.ignacio.gonzam@gmail.com>
2021-02-22 22:27:38 -0300

    Fin etapa uno

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,37 @@
+import { from, gql, useQuery } from '@apollo/client';
 import React from 'react';
 import { Layout } from '../components';
+import TrackCard from '../containers/track-card'
+import QueryResult from '../components/query-result';
 
+const TRACKS = gql`query ExampleQuery {
+  tracksForHome {
+    id
+    title
+    thumbnail
+    length
+    modulesCount
+    author {
+      id
+      name
+      photo
+    }
+  }
+}`
 /**
  * Tacks Page is Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+
+  return <Layout grid> 
+  <QueryResult error={error} loading={loading} data={data}>
+    {data?.tracksForHome.map((track) => (
+      <TrackCard key={track.id} track={track}/>
+    )) } 
+  </QueryResult>
+  </Layout>
 };
 
 export default Tracks;
